7bd6d72df1549b861790f2016e41762263b7d0f5
Kevin Figuera <kevinalejandro3@gmail.com>
2022-09-15 00:30:13 -0400

    Redux DevTools

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,9 @@ import {pokemonsReducer} from "./reducers/pokemons";
 import {Provider} from "react-redux";
 import {legacy_createStore as createStore} from "redux";
 import './index.css'
+const reduxDevToolsConfig = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
-const store = createStore(pokemonsReducer);
+const store = createStore(pokemonsReducer,reduxDevToolsConfig);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
